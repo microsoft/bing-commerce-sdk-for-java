3236abaa903cedda3fbb8023986d9dffd077dac3
Tony Jin <toji@microsoft.com>
2019-04-26 12:38:59 -0700

    add junit and example test

---
+++ src/test/java/com/microsoft/bing/retailsearch/common/ExampleTest.java
@@ -0,0 +1,12 @@
+package com.microsoft.bing.retailsearch.common;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ExampleTest {
+
+    @Test
+    public void exampleTest() {
+        Assertions.assertTrue(true);
+    }
+}
