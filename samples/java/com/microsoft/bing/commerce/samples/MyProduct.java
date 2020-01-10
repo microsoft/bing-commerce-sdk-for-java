789c787322eddbce9d99fc3f6673539468ff9908
almaasrawi <masrawi@outlook.com>
2020-01-10 14:39:50 -0800

    Adding Java Sample (#4)
    * Adding Java Sample
    * Updating the sdk

---
+++ samples/java/com/microsoft/bing/commerce/samples/MyProduct.java
@@ -0,0 +1,19 @@
+package com.microsoft.bing.commerce.samples;
+
+class MyProduct {
+    public String ProductId;
+    public String ProductTitle;
+    public String ProductDescription;
+    public Double ProductPrice;
+    public String arbitraryText;
+    public Double arbitraryNumber;
+
+    public MyProduct(String id, String title, String desc, Double price, String text, Double number) {
+        ProductId = id;
+        ProductTitle = title;
+        ProductDescription = desc;
+        ProductPrice = price;
+        arbitraryText = text;
+        arbitraryNumber = number;
+    }
+}
