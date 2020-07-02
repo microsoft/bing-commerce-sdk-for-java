ee6c92fc6f88eba3275110ea1fe32cf481adc499
Javed Ahmad <v-jaahma@microsoft.com>
2020-07-02 16:25:59 +0530

    Added postRequest inAutoSuggest API

--- samples/java/com/microsoft/bing/commerce/samples/AutoSuggestAPISample.java
+++ samples/java/com/microsoft/bing/commerce/samples/AutoSuggestAPISample.java
@@ -1,6 +1,7 @@
 package com.microsoft.bing.commerce.samples;
 
 import java.io.BufferedReader;
+import java.io.Console;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
@@ -17,6 +18,7 @@ public class AutoSuggestAPISample {
   private final static String INDEX_NAME = System.getenv("Sample_Index");
 
   public static void main(String[] args) throws IOException {
+    postRequest();
   }
 
   public static void request() {
@@ -291,4 +293,58 @@ public class AutoSuggestAPISample {
     System.out.println("POST Response Code :  " + responseCode);
     System.out.println("POST Response Message : " + postConnection.getResponseMessage());
   }
+  
+  public static void postRequest() throws IOException {
+    Dictionary bool_Condition = new Hashtable();
+    bool_Condition.put("_type", "BoolCondition");
+    bool_Condition.put("field", "IsMasterProduct");
+    bool_Condition.put("value", true);
+    bool_Condition.put("Operator", "Eq");
+
+    Dictionary numeric_Condition = new Hashtable();
+    numeric_Condition.put("_type", "NumericCondition");
+    numeric_Condition.put("field", "BasePrice");
+    numeric_Condition.put("value", 80);
+    numeric_Condition.put("Operator", "Ge");
+    
+    Dictionary filter_Dictionary=new Hashtable<>();
+    filter_Dictionary.put("_type", "ConditionBlock");
+    filter_Dictionary.put("Conditions", Arrays.asList(bool_Condition,numeric_Condition));
+    filter_Dictionary.put("Operator", "And");
+    
+    Dictionary Condition = new Hashtable();
+    Condition.put("_type", "StringCondition");
+    Condition.put("field", "Locale");
+    Condition.put("value", "en-us");
+    Condition.put("Operator", "Eq");
+    
+    Dictionary post_filter = new Hashtable();
+    post_filter.put("_type", "ConditionBlock");
+    post_filter.put("Conditions",Arrays.asList(Condition,filter_Dictionary));
+    post_filter.put("Operator", "And");
+    
+    Dictionary POST_PARAMS = new Hashtable();
+    POST_PARAMS.put("Q", "a");
+    POST_PARAMS.put("postFilters",post_filter );
+    
+    String json = new ObjectMapper().writeValueAsString(POST_PARAMS);
+    String customURL = "https://commerce.bing.com/api/autosuggest/v1/"+TENANT_ID+"/indexes/"+INDEX_NAME;
+    URL obj = new URL(customURL);
+    HttpURLConnection postConnection = (HttpURLConnection) obj.openConnection();
+    postConnection.setRequestMethod("POST");
+    postConnection.setRequestProperty("Authorization", ACCESS_TOKEN);
+    postConnection.setRequestProperty("Content-Type", "application/json");
+    postConnection.setDoOutput(true);
+
+    OutputStream os = postConnection.getOutputStream();
+    os.write(json.getBytes());
+    os.flush();
+    os.close();
+
+    int responseCode = postConnection.getResponseCode();
+    System.out.println("POST Response Code :  " + responseCode);
+    System.out.println("POST Response Message : " + postConnection.getResponseMessage());
+  }
+
+
 }
